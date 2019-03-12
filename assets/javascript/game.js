05246266dbc79b279af36d9916d6cd505b3c0f57
Chris Valenzuela <chriss.valenzuela@yahoo.com>
2019-03-12 00:06:28 -0700

    almost done just need to clear wrong guess array better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
 
 
-var wordList = ["chris", "topher", "valenzuela", "san", "chez", "huhhh"];
+var wordList = ["chris", "topher", "valenzuela", "san", "chez", "huhhh", "test", "name", "more", "examples"];
 var chosenWord = "";
 var wrongArray = [];
 var saver = [];
@@ -36,7 +36,7 @@ function wordChooser (){
 
 // the variable "chosenWord" now has a random name. because we run function
 
-var word = wordChooser();
+wordChooser();
 // this funciton checks if a letter is in the random word.
 // parameter "letter" will be used for future
 function checks(letter){
@@ -46,7 +46,6 @@ function checks(letter){
     var seperate = saver.split(" ");
     var letterinWord = false;
     var letternotinWord = false;
-    console.log(wrongArray);
 
     for(var i = 0; i < chosenWord.length; i++){
         if (letter === chosenWord[i]) {
@@ -62,29 +61,70 @@ function checks(letter){
 
     var saver = seperate.join(" ");
     if(letterinWord){
-        correct(saver);
+         correct(saver);
     }
     else if (letternotinWord){
         wrongArray.push(letter);
         wrongGuess(wrongArray);
+
     }    
 
 }
 
 function correct(array){
+    
     document.getElementById("dash").innerHTML = array;
 
+//we seperate and join so that it could be a string with no spaces. 
+    var check = array.split(" ");
+    check = check.join("");
+
+    if( check === chosenWord){
+        document.getElementById("guess").innerHTML = 9;
+        // not completely the way i want to clear the wrong guess but ehh moving on for now
+        wrongArray = [];
+        alert("you win");
+        Wins();
+    }
+    
 }
 
-//addLetter("h");
+
 function wrongGuess(wrongArray){
 
+//parseInt changes the string to an integer so that i can run the for loop
+    var checker = parseInt(document.getElementById("guess").innerHTML);
+
+    document.getElementById("guess").innerHTML = document.getElementById("guess").innerHTML - 1;
     document.getElementById("wrong").innerHTML = "Wrong Guess: " + wrongArray + ",";
+    
+    if(checker === 0){
+        document.getElementById("guess").innerHTML = 9;
+        alert("you lose");
+        // not competely the way i want to clear the wrong guess but ehh moving on for now
+        wrongArray = [];
+        Loss();
+      
+    }
 
 }
 
 
+function Loss(){
+// if u do the "+" it means add in strng not add in integer
+    var x = parseInt(document.getElementById("loss").innerHTML);
+    x = x + 1;
+    document.getElementById("loss").innerHTML = x;
+    wordChooser();
 
+}
+
+function Wins(){
+    var x = parseInt(document.getElementById("wins").innerHTML);
+    x = x + 1;
+    document.getElementById("wins").innerHTML = x;
+    wordChooser();
+}
 
 // this is where user inputs 
 document.onkeyup = function(event){
